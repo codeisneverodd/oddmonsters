180cce1fd5bff4af08bee073c1b470a0a35c43b1
codeisneverodd <codeisneverodd@gmail.com>
2023-06-30 00:03:17 +0900

    feat: add types to logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,19 +1,47 @@
-import Image from "next/image";
-import DarkLogoImage from "/public/logo/transparent/odd-white.svg";
-import LightLogoImage from "/public/logo/transparent/odd-black.svg";
-import { ComponentProps } from "react";
+import OddBlackImage from "/public/logo/transparent/odd-black.svg";
+import OddWhiteImage from "/public/logo/transparent/odd-white.svg";
+import OddMonstersWBlackImage from "/public/logo/transparent/odd-monsters-black.svg";
+import OddMonstersWWhiteImage from "/public/logo/transparent/odd-monsters-white.svg";
+
+import Image, { StaticImageData } from "next/image";
+import { ComponentProps, useMemo } from "react";
 import { twMerge } from "tailwind-merge";
 
 export default function Logo({
   className,
-}: Omit<ComponentProps<typeof Image>, "src" | "alt">) {
+  type = "odd",
+  isInverted = false,
+}: {
+  type: "odd" | "odd-monsters";
+  isInverted?: boolean;
+} & Omit<ComponentProps<typeof Image>, "src" | "alt">) {
+  const targetImage: {
+    dark: StaticImageData;
+    light: StaticImageData;
+  } = useMemo(() => {
+    switch (type) {
+      case "odd":
+        return {
+          dark: OddWhiteImage,
+          light: OddBlackImage,
+        };
+      case "odd-monsters":
+        return {
+          dark: OddMonstersWWhiteImage,
+          light: OddMonstersWBlackImage,
+        };
+    }
+  }, [type]);
   return (
     <picture>
-      <source srcSet={DarkLogoImage.src} media="(prefers-color-scheme: dark)" />
+      <source
+        srcSet={isInverted ? targetImage.light.src : targetImage.dark.src}
+        media="(prefers-color-scheme: dark)"
+      />
       <Image
-        src={LightLogoImage}
+        src={isInverted ? targetImage.dark : targetImage.light}
         className={twMerge("w-24", className)}
-        alt="odd monsters' logo"
+        alt="오드몬스터즈 로고"
         width={160}
         height={40}
       />
