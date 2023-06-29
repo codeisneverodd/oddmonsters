6781fc23a3c71e7555524dfab393a1ef99eefef3
codeisneverodd <codeisneverodd@gmail.com>
2023-06-30 01:17:59 +0900

    feat: change layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,45 @@
 import Image from "next/image";
-import LightBulbManImage from "/public/picture/light-bulb-ink.gif";
 import Link from "next/link";
-import { VStack } from "@/components/base";
+import { ComponentProps } from "react";
+import LightBulbManImage from "/public/picture/light-bulb-ink.gif";
 
 export default function Home() {
   return (
     <main>
       <Thumbnail />
-      <div className="flex"></div>
-      <div className="px-6 py-4 md:px-8">
-        <div className="md:max-w-lg">
-          <h2 className="mb-4 opacity-50">대표 제품</h2>
-          <ArticleCard />
+
+      <div className="m-auto w-full gap-20  px-6 pb-24 pt-10 md:max-w-6xl">
+        <div>
+          <h2 className="mb-4 opacity-50">인기글</h2>
+          <div className="flex w-full flex-wrap gap-8">
+            <ArticleCard />
+          </div>
+        </div>
+        <span className="divider" />
+        <div>
+          <h2 className="mb-4 opacity-50">최신글</h2>
+          <div className="flex w-full flex-wrap gap-8 md:grid md:grid-cols-2 lg:grid-cols-3">
+            <ArticleCard />
+            <ArticleCard />
+            <ArticleCard />
+          </div>
         </div>
       </div>
     </main>
   );
 }
 
-function ArticleCard() {
+function ArticleCard({ className }: ComponentProps<"div">) {
   return (
-    <VStack className="w-full gap-5">
-      <div className="aspect-video w-full rounded-md bg-gray-700" />
+    <div className="group flex w-full cursor-pointer flex-col gap-5">
+      <div className="aspect-video w-full rounded-md bg-gray-700 transition-all group-hover:-translate-y-2" />
       <h4 className="font-light">제품</h4>
-      <h3 className="text-3xl font-bold">티켓팅에 완벽하게 성공하는 법</h3>
+      <h3 className="text-3xl font-bold group-hover:text-blue-500">
+        티켓팅에 완벽하게 성공하는 법
+      </h3>
       <p className="font-light">완벽한 타이밍을 위한 고성능 시계</p>
       <time className="font-light opacity-80">2023.06.29</time>
-    </VStack>
+    </div>
   );
 }
 
@@ -34,6 +47,7 @@ function Thumbnail() {
   return (
     <div className="relative h-[50vh] w-full">
       <Image
+        priority
         src={LightBulbManImage}
         width={400}
         height={400}
