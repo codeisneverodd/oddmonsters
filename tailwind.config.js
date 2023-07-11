9f0d6315f17113fa06cba34c733b1b97739bea0a
codeisneverodd <codeisneverodd@gmail.com>
2023-07-11 15:27:28 +0900

    feat: remove darkmode theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,7 +17,6 @@ module.exports = {
   },
   plugins: [require("@tailwindcss/typography"), require("daisyui")],
   daisyui: {
-    themes: ["lofi", "black"],
-    darkTheme: "black",
+    themes: ["lofi"],
   },
 };
